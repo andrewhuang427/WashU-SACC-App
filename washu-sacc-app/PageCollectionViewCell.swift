a0ad1e81ffde7bf5ac8574efd61eb96ee315bfc3
joewgeorger <joewgeorger@wustl.edu>
2021-11-15 13:24:43 -0600

    CollectionView for home

---
+++ washu-sacc-app/PageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PageCollectionViewCell.swift
+//  washu-sacc-app
+//
+//  Created by Joe Georger on 11/15/21.
+//
+
+import UIKit
+
+class PageCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var pageIcon: UIImageView!
+    @IBOutlet weak var pageLabel: UILabel!
+    
+}
